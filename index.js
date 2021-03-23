0a037489791db39ff39f920c95d0e760cdd5a7c8
Abdul Basir <basir161261@gmail.com>
2021-03-23 07:49:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/',(req, res) =>{
+    res.send("I know how to open Node....YAY")
+})
+app.listen(3000, () => console.log('Listening ro part 3000'));
